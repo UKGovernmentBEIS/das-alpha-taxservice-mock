21eadc4d43b27df2d8f4f82ffd5a1917d2f1efb6
Doug Clinton <doug@wiredthing.com>
2016-09-02 12:45:07 +0100

    The actions in AccessCodeController now use UserAction for authentication

--- src/main/scala/uk.gov/bis/taxserviceMock/controllers/AccessCodeController.scala
+++ src/main/scala/uk.gov/bis/taxserviceMock/controllers/AccessCodeController.scala
@@ -2,21 +2,15 @@ package uk.gov.bis.taxserviceMock.controllers
 
 import javax.inject.Inject
 
-import play.api.mvc.{Action, Controller}
+import play.api.mvc.Controller
 import uk.gov.bis.taxserviceMock.actions.GatewayUserAction
 
 class AccessCodeController @Inject()(UserAction: GatewayUserAction) extends Controller {
-
-  def show(continue: String, origin: Option[String]) = Action { implicit request =>
+  def show(continue: String, origin: Option[String]) = UserAction { implicit request =>
     Ok(views.html.accesscode(continue, origin))
   }
 
-  def handleAccessCode(continue: String, origin: Option[String]) = Action { implicit request =>
-    val r = for {
-      userId <- request.session.get(UserAction.validatedUserKey)
-    } yield Redirect(continue)
-
-    r.getOrElse(Unauthorized)
-
+  def handleAccessCode(continue: String, origin: Option[String]) = UserAction { implicit request =>
+    Redirect(continue)
   }
 }
